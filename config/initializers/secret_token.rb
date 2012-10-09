bd7b7b35dcc305c98190ad90a0f92ee5d21bd778
Carel Du Toit <careld@voxtelecom.co.za>
2012-10-09 14:26:20 +0200

    Added content for refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mosaic::Application.config.secret_token = '6474ee4afc00bca7bf5545ee2f37abed2ac3fc652a8ed2555ea7b0f9c66a289b457f625e1199af357baf270c84f7226317ef752a7fd60c0942c52adf54c2affc'
